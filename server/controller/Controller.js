e42dc9ad384d8423463edd58284eaac5f9dd8749
Grisanu Naing <mgrisanu@gmail.com>
2016-06-20 16:51:08 -0700

    (cleanup) Remove parts of code that still uses mongodb to store seed data

--- server/controller/Controller.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  seedDatabase: function (req, res) {
-    var data = res.body;
-
-    for (var user = 0; user < data.length; user++) {
-      
-    }
-  }
-}
--- server/server.js
+++ server/server.js
@@ -4,11 +4,7 @@ var port = 3000;
 
 require('./config/initialize.js')(app, express);
 
-/* Real Data */
 require('./routes/api-routes.js')(app);
-
-// /* Seed Data */
-// require('./routes/seed-routes.js')(app);
 
 app.listen(port, function() {
     console.log("Listening to Users Service on port: ", port);
