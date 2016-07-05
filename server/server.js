c2b72e231d2cd4e68d8b6980793db30ff62c4919
Grisanu Naing <mgrisanu@gmail.com>
2016-07-05 14:14:44 -0700

    (fix) Fix production env file path

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@
 if (process.env.NODE_ENV === 'development') {
   require('dotenv').config({ path: './server/env/development.env' });
 } else if (process.env.NODE_ENV === 'production') {
-  require('dotenv').config({ path: './server/env/production.env' });
+  require('dotenv').config({ path: './../env/production.env' });
 }
 
 var express = require('express');
